60477bd8dd0f884021cf618f31294025d197f4aa
Henrique Adny <henriqueguinho171@gmail.com>
2022-04-07 10:59:29 -0300

    em js

---
+++ script.js
@@ -0,0 +1,23 @@
+function contar(){
+let inicio = document.getElementById('txti')
+let fim = document.getElementById('txtf')
+let passo = document.getElementById('txtp')
+  
+  if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    window.alert ('[ERRO] faltam dados.')
+  } else {
+      res.innerHTML = 'Contando...'
+      let i = Number (inicio.value)
+      let f = Number (fim.value)
+      let p = Number (passo.value)
+      if (i < f)
+      for (let c = i; c <= f; c += p){
+          res.innerHTML += ` ${c} 👉`
+      } else {
+        for (let c = i; c >= f; c -= p) {
+          res.innerHTML += ` ${c} 👈`
+        }
+      }
+  }
+  
+} 
